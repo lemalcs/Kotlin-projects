3d597d1c18e54d000990277a160c3927397e08b2
Luis Lema <info@lemaluis.com>
2021-08-29 23:37:12 -0500

    FortuneCookie project added.

---
+++ FortuneCookie/src/Fortune.kt
@@ -0,0 +1,28 @@
+fun main()
+{
+   var result: String=getFortuneCookie()
+    println("Your fortune is: $result")
+}
+
+fun getFortuneCookie():String
+{
+    val messages=listOf("You will have a great day!",
+        "Things will go well for you today.",
+        "Enjoy a wonderful day of success.",
+        "Be humble and all will turn out well.",
+        "Today is a good day for exercising restraint.",
+        "Take it easy and enjoy life!",
+        "Treasure your friends because they are your greatest fortune.")
+
+    println("Enter your birthday")
+    var birthday=readLine()
+    var birthdayNumber:Int=birthday?.toIntOrNull()?:1
+
+    if (birthdayNumber==0)
+        birthdayNumber = 1
+
+    val fortune:Int=birthdayNumber%messages.size
+    println(fortune)
+
+    return messages[fortune]
+}
